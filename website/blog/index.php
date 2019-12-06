3c5dac9835ff71f98c090ff357dcd28fe498ec53
hui-ho <hui-ho@outlook.com>
2019-12-06 15:11:21 +0800

    安装typecho程序

--- website/blog/index.php
+++ website/blog/index.php
@@ -1,14 +1,26 @@
 <?php
-$db_type = 'mysql';
-$host = 'mysql';
-$db_port = '3306';
-$db_name = 'blog';
-$user = 'blog';
-$passwd = 'blog';
-$dsn = "$db_type:host=$host;port=$db_port;dbname=$db_name";
-try {
-    $db_pdo = new PDO($dsn, $user, $passwd);
-    echo 'successful<br/>';
-} catch (PDOException $e) {
-    die('Error!: ' . $e->getMessage() . '<br/>');
+/**
+ * Typecho Blog Platform
+ *
+ * @copyright  Copyright (c) 2008 Typecho team (http://www.typecho.org)
+ * @license    GNU General Public License 2.0
+ * @version    $Id: index.php 1153 2009-07-02 10:53:22Z magike.net $
+ */
+
+/** 载入配置支持 */
+if (!defined('__TYPECHO_ROOT_DIR__') && !@include_once 'config.inc.php') {
+    file_exists('./install.php') ? header('Location: install.php') : print('Missing Config File');
+    exit;
 }
+
+/** 初始化组件 */
+Typecho_Widget::widget('Widget_Init');
+
+/** 注册一个初始化插件 */
+Typecho_Plugin::factory('index.php')->begin();
+
+/** 开始路由分发 */
+Typecho_Router::dispatch();
+
+/** 注册一个结束插件 */
+Typecho_Plugin::factory('index.php')->end();
